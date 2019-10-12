f481a4f037b8ee645f27385a42f1139504c62624
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-12 17:37:02 +0200

    adding handling error in Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,7 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null,
-        name: 'Bla'
+        error: false
     };
 
 
@@ -25,7 +25,9 @@ class Blog extends Component {
                     }
                 });
                 this.setState({posts: updatePosts});
-            })
+            }).catch(error => {
+                this.setState({error: true})
+        })
     }
 
     postSelectedHandler = (id) => {
@@ -33,13 +35,17 @@ class Blog extends Component {
     };
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}/>
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+
+        if(!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
 
         return (
             <div>
