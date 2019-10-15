3d94b319acdaf1b3a9226ba73f7808b67b54a1b9
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-10-15 09:04:11 +0200

    addding router, routerLink to project

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import './Blog.css';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
+import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
 
@@ -13,12 +14,21 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration : 'underline'
+                                }}
+
+                                >Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-posts',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
@@ -26,6 +36,7 @@ class Blog extends Component {
                 {/*<Route path="/"  render={() => <h1>Home2</h1>} />*/}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-posts"  component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
